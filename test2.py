cd4856ceb43ed10fdcdbd1a616d02be9470d5570
SCBera <subhas.9874@gmail.com>
2019-05-29 14:28:50 +0200

    fig.close() not working

--- test2.py
+++ test2.py
@@ -21,6 +21,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget
 from numpy import arange, sin, pi
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
 
@@ -31,7 +32,8 @@ class MyMplCanvas(FigureCanvas):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
+        # self.axes.hold(False) # depricated
+        # self.axes.clear() # not working
 
         self.compute_initial_figure()
 
@@ -43,6 +45,8 @@ class MyMplCanvas(FigureCanvas):
                                    QSizePolicy.Expanding,
                                    QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
+
+        self.toolbar = NavigationToolbar(self)
 
     def compute_initial_figure(self):
         pass
@@ -81,7 +85,7 @@ class ApplicationWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setWindowTitle("application main window")
+        self.setWindowTitle("Application main window")
 
         self.file_menu = QMenu('&File', self)
         self.file_menu.addAction('&Quit', self.fileQuit,
@@ -95,6 +99,8 @@ class ApplicationWindow(QMainWindow):
         self.help_menu.addAction('&About', self.about)
 
         self.main_widget = QWidget(self)
+
+        # self.toolbar = 
 
         l = QVBoxLayout(self.main_widget)
         sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
