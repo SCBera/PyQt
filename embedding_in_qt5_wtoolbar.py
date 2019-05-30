2a7dec89c7e869cad23d5ca59e6dadfd054e6533
Subhas Ch Bera <subhas.9874@gmail.com>
2019-05-30 10:39:02 +0200

    layout better

--- embedding_in_qt5_wtoolbar.py
+++ embedding_in_qt5_wtoolbar.py
@@ -18,57 +18,144 @@ from matplotlib.backends.backend_qt5agg import (
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import random
 
 
-class AppForm(QMainWindow):
-    def __init__(self, parent=None):
-        QMainWindow.__init__(self, parent)
+class window(QMainWindow):
+    def __init__(self, width, height, parent=None):
+        super(window, self).__init__(parent)
+        # QMainWindow.__init__(self, parent)
+
+        self.xpos = 50
+        self.ypos = 50
+        self.width = width
+        self.height = height
+        # self.width = 500
+        # self height = 300
+
+        self.setGeometry(self.xpos, self.ypos, self.width/2, self.height/2)
+        self.setWindowTitle('Test!')
+
         #self.x, self.y = self.get_data()
-        self.data = self.get_data()
+        # self.data = self.get_data()
         self.create_main_frame()
-        self.on_draw()
+        # self.on_draw()
 
+
+        ## to open file ##
+        openFile = QAction('&Open file', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open file')
+        openFile.triggered.connect(self.open_file)
+
+        ## to save file ##
+        saveFile = QAction('&Save file', self)
+        saveFile.setShortcut('Ctrl+S')
+        saveFile.setStatusTip('Save file')
+        saveFile.triggered.connect(self.save_file)
+
+        ## to quite ##
+        quitAction = QAction('&Exit ', self)
+        quitAction.setShortcut('Ctrl+Q')
+        quitAction.setStatusTip('Close the window!')
+        quitAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(openFile)
+        fileMenu.addAction(saveFile)
+        fileMenu.addAction(quitAction)
+
+        test_btn = QPushButton('Test')
+
+        test_btn.resize(test_btn.sizeHint())  # size of the button (width, hight)
+        test_btn.move(self.width - 100, self.height - 40)  # location
 
     def create_main_frame(self):
         self.main_frame = QWidget()
 
-        self.fig = Figure((6.0, 4.0), dpi=100)
+        # self.fig = Figure()
+        self.fig = Figure((3.0, 2.0), dpi=100)
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self.main_frame)
-        self.canvas.setFocusPolicy(Qt.StrongFocus)
-        self.canvas.setFocus()
+        # self.canvas.setFocusPolicy(Qt.StrongFocus)
+        # self.canvas.setFocus()
 
         self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
 
         self.canvas.mpl_connect('key_press_event', self.on_key_press)
 
-        self.btn1 = QPushButton('Test button 1')
+
+
+
+        self.btn_plot = QPushButton('Plot')
+        self.btn_plot.clicked.connect(self.plot_static)
+        self.btn_plot_nxt = QPushButton('Plot next')
+        self.btn_plot_nxt.clicked.connect(self.on_draw)
+        self.exit_btn = QPushButton('Exit!')
+        self.exit_btn.clicked.connect(self.close_application)
+
+        mainHbox = QHBoxLayout()
+
+        self.main_frame.setLayout(mainHbox)
+        self.setCentralWidget(self.main_frame)
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.canvas)  # the matplotlib canvas
         vbox.addWidget(self.mpl_toolbar)
 
-        vbox.addWidget(self.btn1)
+        # vbox.addWidget(self.btn_plot)
+        vbox.addWidget(self.btn_plot_nxt)
 
-        self.main_frame.setLayout(vbox)
-        self.setCentralWidget(self.main_frame)
+        # mainHbox = QHBoxLayout()
+
+        # self.main_frame.setLayout(mainHbox)
+        # self.setCentralWidget(self.main_frame)
+
+        mainHbox.addLayout(vbox)
+        # hbox.addWidget(self.exit_btn)
+        mainHbox.addWidget(self.btn_plot)
+        # mainHbox.addWidget(self.btn_plot_nxt)
+        # mainHbox.addWidget(self.exit_btn)
 
 
-        ## not working...
-        self.btn2 = QPushButton('Test button 2')
 
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.btn2)
+
+
+    def open_file(self):
+        name, _ = QFileDialog.getOpenFileName(self, 'Open file')
+        # name, _ = QFileDialog.getOpenFileName(self, 'Open file', option=QFileDialog.DontUseNativeDialog)
+
+        print('file name:', name)
+        # self.editor()
+        # with open(name, 'r') as f:  # this will close the file after use.
+            # text = f.read()
+            # self.textEdit.setText(text)
+
+    def save_file(self):
+        name, _ = QFileDialog.getSaveFileName(self, 'Save file')
+        pass
+
 
     def get_data(self):
-        return np.arange(20).reshape([4, 5]).copy()
+        return [random.random() for i in range(10)]
+        # return np.arange(20).reshape([4, 5]).copy() # for imshow
 
-    def on_draw(self):
+    def plot_static(self):
         self.fig.clear()
         self.axes = self.fig.add_subplot(111)
-        #self.axes.plot(self.x, self.y, 'ro')
-        self.axes.imshow(self.data, interpolation='nearest')
-        #self.axes.plot([1,2,3])
+        self.axes.plot(range(10), 'o-')
+        # self.axes.imshow(self.data, interpolation='nearest')
+        self.canvas.draw()
+
+    def on_draw(self):
+        self.data = self.get_data()
+        self.fig.clear() # this will clear the old plot if recalled
+        self.axes = self.fig.add_subplot(111)
+        self.axes.plot(self.data, 'o-')
+        # self.axes.imshow(self.data, interpolation='nearest')
         self.canvas.draw()
 
     def on_key_press(self, event):
@@ -77,10 +164,27 @@ class AppForm(QMainWindow):
         # http://matplotlib.org/users/navigation_toolbar.html#navigation-keyboard-shortcuts
         key_press_handler(event, self.canvas, self.mpl_toolbar)
 
+    def close_application(self):
+
+        choice = QMessageBox.question(self, 'Message',
+                                      "Are you sure to quit?", QMessageBox.Yes |
+                                      QMessageBox.No, QMessageBox.No)
+
+        if choice == QMessageBox.Yes:
+            print('Quit application')
+            sys.exit()
+        else:
+            pass
+
 
 def main():
     app = QApplication(sys.argv)
-    form = AppForm()
+    screen = app.primaryScreen()
+    screen_sz = screen.size()  # returns the current system screen size
+    width = screen_sz.width()
+    height = screen_sz.height()
+    print(screen_sz)
+    form = window(width, height)
     form.show()
     app.exec_()
 
